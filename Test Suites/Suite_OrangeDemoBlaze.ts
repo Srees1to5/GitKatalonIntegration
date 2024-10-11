c99666327338e58c9bcd041ce75b43dc61bb1362
Anoop_G <Anoop_G@192.168.1.38>
2024-10-11 18:37:39 +0530

    oct11th

---
+++ Test Suites/Suite_OrangeDemoBlaze.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite_OrangeDemoBlaze</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c953a44a-243c-47c4-a32c-a88f537f50e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>7b96302c-5aa1-4736-9230-d3c5738a7187</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoBlazeTagsAdded</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c2a0eab2-e9e2-41f1-b24f-c65169418414</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/dempapplotz</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
